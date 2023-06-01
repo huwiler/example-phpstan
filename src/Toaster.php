b077226030419a522c4aec066146df90ff09920a
Matt Huwiler <huwilerm@champlain.edu>
2023-06-01 15:29:38 -0400

    nice developer fixes bugs

--- src/Toaster.php
+++ src/Toaster.php
@@ -7,7 +7,7 @@ class Toaster
     public function toast(int $time = 1): string
     {
         if ($time > 3) {
-            return "You burned me!;
+            return "You burned me!";
         }
 
         return "Yay toast!";
