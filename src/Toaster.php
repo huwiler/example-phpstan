82d9c0c818cf1a379bbb7b1259ec9dfe03e1e5d7
Matt Huwiler <huwilerm@champlain.edu>
2023-06-01 15:18:29 -0400

    new toaster added

---
+++ src/Toaster.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Toaster
+{
+    public function toast(int $time = 1): string
+    {
+        if ($time > 3) {
+            return "You burned me!";
+        }
+
+        return "Yay toast!";
+    }
+}
